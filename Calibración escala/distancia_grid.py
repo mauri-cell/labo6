e5faa94b66ea3133b372735700f689c38369352c
mauri_stack <mauriivan10@gmail.com>
2026-06-03 17:02:01 -0300

    analisis grid de varios perfiles

--- Calibración escala/distancia_grid.py
+++ Calibración escala/distancia_grid.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 #Ajustar la prominence y la distance hasta que sean los mínimos que queremos
 
 #%%
 # Ruta al csv
-ruta_csv = "/Users/Mauri/Downloads/perfil_grilla.csv"
+ruta_csv = "/Users/Mauri/Downloads/perfiles grilla /perfil 6.csv"
 
 # Leer archivo
 df = pd.read_csv(ruta_csv)
@@ -20,7 +21,7 @@ y = df.iloc[:, 1].to_numpy()
 
 # Buscar mínimos relativos
 # prominence ayuda a ignorar ruido
-indices_minimos, _ = find_peaks(-y, prominence=4000)
+indices_minimos, _ = find_peaks(-y, prominence=6000, distance = 50)
 
 # Posiciones de los mínimos en píxeles
 minimos_x = x[indices_minimos]
@@ -57,3 +58,64 @@ plt.legend()
 plt.show()
 
 
+#%% Hacerlo para varios perfiles
+
+# Carpeta con los csv
+carpeta = Path("/Users/Mauri/Downloads/perfiles grilla ")
+
+# Lista donde guardar todas las diferencias
+todas_las_diferencias = []
+
+# Opcional: guardar resultados archivo por archivo
+resultados = {}
+
+for archivo in carpeta.glob("*.csv"):
+
+    # Leer csv
+    df = pd.read_csv(archivo)
+
+    x = df.iloc[:, 0].to_numpy()
+    y = df.iloc[:, 1].to_numpy()
+
+    # Buscar mínimos
+    indices_minimos, _ = find_peaks(
+        -y,
+        prominence=6000, distance = 50
+    )
+
+    minimos_x = x[indices_minimos]
+
+    # Diferencias entre mínimos
+    diferencias = np.diff(minimos_x)
+
+    # Guardar para este archivo
+    resultados[archivo.name] = diferencias
+
+    # Agregar a la lista global
+    todas_las_diferencias.extend(diferencias)
+
+# Convertir a numpy array
+todas_las_diferencias = np.array(todas_las_diferencias)
+
+# Estadísticas globales
+promedio_global = np.mean(todas_las_diferencias)
+desv_std_global = np.std(todas_las_diferencias, ddof=1)
+error_media_global = desv_std_global / np.sqrt(len(todas_las_diferencias))
+
+print(f"Promedio global = {promedio_global:.3f} px")
+#print(f"Desv. estándar = {desv_std_global:.3f} px")
+#print(f"Error de la media = {error_media_global:.3f} px")
+print(f"Número total de períodos = {len(todas_las_diferencias)}")
+
+print(f"Período promedio = ({promedio_global:.2f} ± {error_media_global:.2f}) px")
+
+#%%para ver qué dió cada archivo
+
+for nombre, diferencias in resultados.items():
+
+    promedio = np.mean(diferencias)
+    error = np.std(diferencias, ddof=1) / np.sqrt(len(diferencias))
+
+    print(f"{nombre}: {promedio:.2f} ± {error:.2f} px")
+    
+    
